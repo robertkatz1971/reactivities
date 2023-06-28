25c5479b117453f2bfa35e4c9b29afe38f0dc87b
rkatz-evertrue <robert.katz@evertrue.com>
2023-06-28 19:50:34 -0400

    initial commit (end of section 2)

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,20 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context) => _context = context;
+
+        [HttpGet] //api/activities
+        public async Task<ActionResult<List<Activity>>> GetActivities() => await _context.Activities.ToListAsync();
+
+        [HttpGet("{id}")] //api/activities/guidValue
+        public async Task<ActionResult<Activity>> GetActivity(Guid id) => await _context.Activities.FindAsync(id);
+    }
+}
